c9849737f592877a3434e3e49f344759cde87bb9
Jesse Robinson <45837967+jrobinson0529@users.noreply.github.com>
2021-03-23 10:36:20 -0500

    setup files

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -7,9 +7,6 @@ import '../styles/main.scss';
 const init = () => {
   document.querySelector('#app').innerHTML = '<h1>HELLO! You are up and running!</h1>';
   console.warn('YOU ARE UP AND RUNNING!');
-
-  // USE WITH FIREBASE AUTH
-  // checkLoginStatus();
 };
 
 init();
